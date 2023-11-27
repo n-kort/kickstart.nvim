203989d697da75ec20de8d8eb55e02a1723809af
N Kort <studio@n-kort.net>
2023-11-27 14:06:02 +0100

    asterisk plugin

---
+++ lua/custom/plugins/lasterisk.lua
@@ -0,0 +1,8 @@
+return {
+  'rapan931/lasterisk.nvim',
+  init = function()
+    vim.keymap.set('n', '*', function() require("lasterisk").search() end)
+    vim.keymap.set('n', 'g*', function() require("lasterisk").search({ is_whole = false }) end)
+    vim.keymap.set('x', 'g*', function() require("lasterisk").search({ is_whole = false }) end)
+  end,
+}
