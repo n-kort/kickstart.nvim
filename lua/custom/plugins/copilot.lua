47c656f9ae6a1f672160dbf2f99822d43d1f1787
N Kort <studio@n-kort.net>
2024-02-23 15:21:27 +0100

    enable yml

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,8 +3,11 @@ return {
   init = function()
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
+    vim.g.copilot_filetypes = {
+      ['yml'] = true,
+      ['yaml'] = true,
+    }
     vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    
   end,
 }
 
