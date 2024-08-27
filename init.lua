91d3cf34968ea022adba802251d2bfe255cef2d9
N Kort <studio@n-kort.net>
2024-08-27 11:38:14 +0200

    updates after which-key changes

--- init.lua
+++ init.lua
@@ -590,24 +590,92 @@ require('lazy').setup({
     end,
   },
 
-  -- Useful plugin to show you pending keybinds.
-  {
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter',
-    config = function()
-      require('which-key').setup()
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    opts = {
+      icons = {
+        -- set icon mappings to true if you have a Nerd Font
+        mappings = vim.g.have_nerd_font,
+        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+        -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+        keys = vim.g.have_nerd_font and {} or {
+          Up = '<Up> ',
+          Down = '<Down> ',
+          Left = '<Left> ',
+          Right = '<Right> ',
+          C = '<C-…> ',
+          M = '<M-…> ',
+          D = '<D-…> ',
+          S = '<S-…> ',
+          CR = '<CR> ',
+          Esc = '<Esc> ',
+          ScrollWheelDown = '<ScrollWheelDown> ',
+          ScrollWheelUp = '<ScrollWheelUp> ',
+          NL = '<NL> ',
+          BS = '<BS> ',
+          Space = '<Space> ',
+          Tab = '<Tab> ',
+          F1 = '<F1>',
+          F2 = '<F2>',
+          F3 = '<F3>',
+          F4 = '<F4>',
+          F5 = '<F5>',
+          F6 = '<F6>',
+          F7 = '<F7>',
+          F8 = '<F8>',
+          F9 = '<F9>',
+          F10 = '<F10>',
+          F11 = '<F11>',
+          F12 = '<F12>',
+        },
+      },
 
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
+      -- Document existing key chains
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    },
   },
+  -- Useful plugin to show you pending keybinds.
+  -- {
+  --   'folke/which-key.nvim',
+  --   event = 'VimEnter',
+  --   config = function()
+  --     require('which-key').setup()
+  --
+  --     require('which-key').register {
+  --       -- { "<leader>c", group = "[C]ode" },
+  --       -- { "<leader>c_", hidden = true },
+  --       -- { "<leader>d", group = "[D]ocument" },
+  --       -- { "<leader>d_", hidden = true },
+  --       -- { "<leader>g", group = "[G]it" },
+  --       -- { "<leader>g_", hidden = true },
+  --       -- { "<leader>h", group = "More git" },
+  --       -- { "<leader>h_", hidden = true },
+  --       -- { "<leader>r", group = "[R]ename" },
+  --       -- { "<leader>r_", hidden = true },
+  --       -- { "<leader>s", group = "[S]earch" },
+  --       -- { "<leader>s_", hidden = true },
+  --       -- { "<leader>w", group = "[W]orkspace" },
+  --       -- { "<leader>w_", hidden = true },
+  --       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+  --       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+  --       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+  --       ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
+  --       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+  --       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+  --       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  --     }
+  --   end,
+  -- },
+
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
