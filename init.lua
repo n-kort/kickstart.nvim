49e92ad1d6a99815729a16b93f8c1bffdc3a3b00
N Kort <studio@n-kort.net>
2024-10-01 11:29:07 +0200

    php stubs

--- init.lua
+++ init.lua
@@ -360,7 +360,15 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {
+        intelephense = {
+          filetypes = { 'php' },
+          settings = {
+            intelephense = {
+              stubs = { "bcmath", "bz2", "Core", "curl", "date", "dom", "fileinfo", "filter", "gd", "gettext", "hash", "iconv", "imap", "intl", "json", "libxml", "mbstring", "mcrypt", "mysql", "mysqli", "password", "pcntl", "pcre", "PDO", "pdo_mysql", "Phar", "readline", "regex", "session", "SimpleXML", "sockets", "sodium", "standard", "superglobals", "tokenizer", "xml", "xdebug", "xmlreader", "xmlwriter", "yaml", "zip", "zlib", "wordpress", "wordpress-stubs", "woocommerce-stubs", "acf-pro-stubs", "wordpress-globals", "wp-cli-stubs", "genesis-stubs", "polylang-stubs" },
+            }
+          }
+        },
+
         ts_ls = {
           init_options = {
             plugins = {
