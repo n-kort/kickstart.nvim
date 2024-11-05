7d98cb76e11d8e2f7c2bcc8df72397374f08a546
N Kort <studio@n-kort.net>
2024-11-05 16:45:37 +0100

    gitsigns updates for blame etc

--- init.lua
+++ init.lua
@@ -687,44 +687,6 @@ require('lazy').setup({
   -- },
 
   {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
-
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        vim.keymap.set({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-        vim.keymap.set({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-      end,
-    },
-  },
-
-  {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
@@ -773,7 +735,6 @@ require('lazy').setup({
   -- Fuzzy Finder (files, lsp, etc)
   {
     'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
     event = 'VimEnter',
     branch = '0.1.x',
     dependencies = {
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -10,4 +10,3 @@ return {
     vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
 }
-
