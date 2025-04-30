b529c232b1e6d8365b5237b3af88b7f236d25f20
N Kort <studio@n-kort.net>
2025-04-30 15:43:38 +0200

    deno_ls different

--- init.lua
+++ init.lua
@@ -399,16 +399,16 @@ require('lazy').setup({
           end,
         },
 
-        denols = {
-          on_attach = function(client)
-            if require('lspconfig').util.root_pattern('package.json')(vim.fn.getcwd()) then
-              if client.name == 'denols' then
-                client.stop()
-                return
-              end
-            end
-          end,
-        },
+        -- denols = {
+        --   on_attach = function(client)
+        --     if require('lspconfig').util.root_pattern('package.json')(vim.fn.getcwd()) then
+        --       if client.name == 'denols' then
+        --         client.stop()
+        --         return
+        --       end
+        --     end
+        --   end,
+        -- },
 
         volar = {
           filetypes = { 'vue' },
