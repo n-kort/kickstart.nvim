80d486cfbe96adb434bec72a06ecb5032a73add5
N Kort <studio@n-kort.net>
2025-06-05 14:15:27 +0200

    new copilot

---
+++ lua/custom/plugins/copilot-2.lua
@@ -0,0 +1,19 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-j>",
+          next = "<C-l>",
+          prev = "<C-h>",
+          dismiss = "<C-k>"
+        }
+      },
+      file
+    })
+  end,
+}
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,12 +1,13 @@
 return {
   "github/copilot.vim",
-  init = function()
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_filetypes = {
-      ['yml'] = true,
-      ['yaml'] = true,
-    }
-    vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  end,
+  enabled = false,
+  -- init = function()
+  --   vim.g.copilot_no_tab_map = true
+  --   vim.g.copilot_assume_mapped = true
+  --   vim.g.copilot_filetypes = {
+  --     ['yml'] = true,
+  --     ['yaml'] = true,
+  --   }
+  --   vim.api.nvim_set_keymap('i', '<C-e>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  -- end,
 }
