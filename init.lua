7375b7b0ddb33bde69830c8ed507fb736055dfd1
N Kort <studio@n-kort.net>
2025-11-12 10:12:28 +0100

    disable ts_ls for vue

--- init.lua
+++ init.lua
@@ -574,7 +574,7 @@ require('lazy').setup({
 
       vim.lsp.config('vtsls', vtsls_config)
       vim.lsp.config('vue_ls', vue_ls_config)
-      vim.lsp.config('ts_ls', ts_ls_config)
+      -- vim.lsp.config('ts_ls', ts_ls_config)
       vim.lsp.enable({ 'vtsls', 'vue_ls' })
 
       vim.lsp.config('lua_ls', {
